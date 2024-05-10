c366b223e078d031c23f4874efa793c7c7ab085c
Poglia <pedro.poglia@hotmail.com>
2024-05-10 13:33:22 -0300

    feat: create controllers/apiController

---
+++ src/controllers/apiController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+
+import { Phrase } from "../models/phrase";
+
+export const ping = (req: Request, res: Response) => {
+    res.json({pong: true})
+}
+
+export const random = (req: Request, res: Response) => {
+
+    let nRand: number = Math.floor(Math.random() * 10)
+    res.json({number: nRand});
+}
+
+export const nome = (req: Request, res: Response) => {
+
+    let nome: string = req.params.nome
+    res.json({nome});
+}
