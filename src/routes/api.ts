2e6ea990d03fe037df637eadca9fbfb2fb625adc
Poglia <pedro.poglia@hotmail.com>
2024-05-10 13:59:31 -0300

    feat: alter api.ts

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,21 +1,13 @@
 import { Router } from "express";
 
+import * as ApiController from "../controllers/apiController";
+
 const router = Router();
 
-router.get('/ping', (req, res) => {
-    res.json({pong: true});
-})
+router.get('/ping', ApiController.ping);
 
-router.get('/random', (req, res) => {
+router.get('/random', ApiController.random);
 
-    let nRand: number = Math.floor(Math.random() * 10)
-    res.json({number: nRand});
-})
-
-router.get('/nome/:nome', (req, res) => {
-
-    let nome: string = req.params.nome
-    res.json({nome});
-})
+router.get('/nome/:nome', ApiController.nome)
 
 export default router;
