960c28ab4e423bf0e2acce5ab7340be191a6a426
Poglia <pedro.poglia@hotmail.com>
2024-07-15 08:44:21 -0300

    feat: add swagger lines no server.js

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,9 @@ import path from 'path';
 import dotenv from 'dotenv';
 import apiRoutes from './routes/api'
 import cors from 'cors';
+import swaggerUi from "swagger-ui-express"
+
+import swaggerDocs from '../swagger.json'
 
 dotenv.config();
 
@@ -16,6 +19,7 @@ server.use(express.static(path.join(__dirname, '../public')));
 server.use(express.urlencoded({extended: true}));
 
 server.use('/api', apiRoutes);
+server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 server.use((req: Request, res: Response) => {
     res.status(404);
